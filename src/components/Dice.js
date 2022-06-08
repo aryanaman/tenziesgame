58ffc698c08262a31109e0fb61da64da6ae1f334
aryanaman <amanaryankr@gmail.com>
2022-06-08 15:07:07 +0530

    added image for git

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -8,11 +8,11 @@ function Dice(props) {
   };
   return (
     <div className="dice" onClick={props.holdDice}>
-      <img src={`./Terning${props.value}.svg`} style={styles} alt="dice_img" />
-      {/* <h2 className="dice-num" style={styles}>
+      {/* <img src={`./Terning${props.value}.svg`} style={styles} alt="dice_img" /> */}
+      <h2 className="dice-num" style={styles}>
         {" "}
         {props.value}
-      </h2> */}
+      </h2>
     </div>
   );
 }
